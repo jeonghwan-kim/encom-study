20cf58252156d30db7eff1c1ad6d81f90d88c96a
jeonghwan-kim <ej88ej@gmail.com>
2016-11-05 17:16:15 +0900

    form 1

--- js/directives/todoForm.js
+++ js/directives/todoForm.js
@@ -5,11 +5,19 @@ angular.module('todomvc').directive('todoForm', () => {
       onCreate: '&'
     },
     template: `
-      <input type="text" ng-model="newTodoTitle" />
-      <button ng-click="onCreate({title: newTodoTitle})">Add</button>
+      <form name="todoForm" ng-submit="onCreate({title: newTodoTitle})">
+        <input type="text"
+               name="title"
+               ng-model="newTodoTitle"
+               ng-minlength="3" />
+             <input type="text" name="foo">
+        <input type="submit" value="Add" ng-disabled="!todoForm.title.$modelValue" />
+      </form>
+      <pre>{{todoForm|json}}</pre>
+      <p ng-if="todoForm.title.$error.minlength">3자리 이상 입력하세요.</p>
       `,
      link: (scope, elem, attr) => {
-       scope.newTodoTitle =  'foo';
+       scope.newTodoTitle =  '';
      }
   }
 });

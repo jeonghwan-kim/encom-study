ea7867e0477de7fb2cc7d8e064d242f98acb9fe3
jeonghwan-kim <ej88ej@gmail.com>
2016-11-19 12:56:00 +0900

    fixed createdAt1 bug

--- js/directives/todoItem.js
+++ js/directives/todoItem.js
@@ -9,11 +9,11 @@ angular.module('todomvc').directive('todoItem', ($filter) => {
     template: `
       <input type="checkbox" ng-model="data.done" ng-click="onUpdate({todo: data})" />
       <input type="text" ng-model="data.title" ng-blur="onUpdate({todo: data})" />
-      <date>{{ createdAt2 }}</date>
+      <date>{{createdAt2()}}</date>
       <button ng-click="onRemove({todo: data})">Remove</button>
       `,
     link: (scope) => {
-      scope.createdAt2 = [
+      scope.createdAt2 = ()=> [
         $filter('dateKo')(scope.data.createdAt),
         $filter('timeKo')(scope.data.createdAt)
       ].join(' ');
